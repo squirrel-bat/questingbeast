25780f651c63bd2726e6b5e279ebe3a6491359f1
squirrel-bat <8386773+squirrel-bat@users.noreply.github.com>
2022-11-15 23:45:42 +0100

    Remove unused JS file

--- dist/beast.js
+++
@@ -1,7 +0,0 @@
-function crashScryfall(elem) {
-  document.getElementById("questingbeast").removeAttribute("src");
-  const crashedDiv = document.createElement("div");
-  crashedDiv.innerText = "Scryfall successfully crashed!";
-  crashedDiv.classList.add("center");
-  elem.replaceWith(crashedDiv);
-}
