b4bcbb52cd37962800f7fd0650ecf90ef70d6cf3
squirrel-bat <8386773+squirrel-bat@users.noreply.github.com>
2022-11-16 23:08:09 +0100

    Implement menu

--- dist/beast.js
+++ dist/beast.js
@@ -1,3 +1,32 @@
+function toggleDetails() {
+  document.getElementById("details").classList.toggle("hidden");
+  document.querySelectorAll(".btn-default span").forEach((el) => {
+    el.classList.toggle("display-none");
+  });
+  if (!document.getElementById("details").classList.contains("hidden")) {
+    window.scrollTo({
+      left: 0,
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  }
+}
+
+function toggleMenuState() {
+  document.querySelectorAll(".menu").forEach((el) => {
+    el.classList.toggle("display-none");
+  });
+}
+
+function showTabById(id) {
+  toggleMenuState();
+  document.getElementById(id).classList.toggle("display-none");
+}
+
+function reset() {
+  location.reload();
+}
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
